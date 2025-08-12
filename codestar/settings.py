596f4e71a0d637704d1e469e626118628fb7c01d
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-08-12 13:06:51 +0100

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
