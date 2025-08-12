6b90b7cd8dd62bb6ade5eb64c4e33926585cbfb7
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-08-12 17:31:21 +0100

    troubleshoot error message by adding allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,10 +29,11 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '.herokuapp.com'
+    '.herokuapp.com',
+    '127.0.0.1',
 ]
 
 # Application definition
