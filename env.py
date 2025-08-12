06738965d3b176f923edb77b6927e8b4ef11d857
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-08-12 14:33:10 +0100

    set up the superuser admin settings

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_Z5qWhyS0RYNf@ep-quiet-math-a21ksfdl."
+    "eu-central-1.aws.neon.tech/apron_cramp_zero_941675"
+)
