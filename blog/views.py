14abb6f4dc9be19122ff5e9098c1c9bab7e97d7f
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-08-19 16:47:27 +0100

    create a collaboration form using crispy forms

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -32,6 +33,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
+    print("about to render template")
 
     return render(
         request,
